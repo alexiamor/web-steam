b36efe870543c0510e36c359153336a51ca67399
alexiamor <alexiamorenoguevara@gmail.com>
2024-06-07 22:25:33 +0200

    Web final

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,76 +1,60 @@
 <template>
-    <footer class="footer">
-      <div class="footer-left">
-        <p>For any questions, please contact steam@support.com</p>
-        <div class="iconos">
-            <div class="user" @click="panelActive = !panelActive"><Icon name="line-md:account" size="2em" color="black"></Icon></div>
-            <div class="user" @click="panelActive = !panelActive"><Icon name="line-md:account" size="2em" color="black"></Icon></div>
-            <div class="user" @click="panelActive = !panelActive"><Icon name="line-md:account" size="2em" color="black"></Icon></div>
-            <div class="user" @click="panelActive = !panelActive"><Icon name="line-md:account" size="2em" color="black"></Icon></div>
-            <div class="user" @click="panelActive = !panelActive"><Icon name="line-md:account" size="2em" color="black"></Icon></div>
+  <div class="flex flex-col bg-g200 gap-24 p-14">
+    <div class="flex justify-between">
+      <p class="text-primary">
+        Para cualquier pregunta, por favor, contacta con steam@support.com
+      </p>
+      <div class="flex gap-6">
+        <div class="flex flex-col gap-3">
+          <NuxtLink to="/"><h5>Tienda</h5></NuxtLink>
+          <NuxtLink to="/"><p>Nuevos y destacables</p></NuxtLink>
+          <NuxtLink to="/"><p>Juegos por tipo</p></NuxtLink>
+          <NuxtLink to="/"><p>Ofertas</p></NuxtLink>
+          <NuxtLink to="/"><p>Tienda de puntos</p></NuxtLink>
         </div>
-        <p class="footer-rights">@2024 Steam. All rights reserved</p>
-      </div>
-      <div class="footer-right">
-        <div class="columna">
-          <h3>Tienda</h3>
-          <ul>
-            <!-- Aquí colocarías tus 5 opciones de la tienda -->
-          </ul>
+        <div class="flex flex-col gap-3">
+          <NuxtLink to="/comunidadNoticias"><h5>Comunidad</h5></NuxtLink>
+          <NuxtLink to="/comunidadNoticias"><p>Noticias</p></NuxtLink>
+          <NuxtLink to="/comunidadDiscusiones"><p>Discusiones</p></NuxtLink>
+          <NuxtLink to="/comunidadStream"><p>Streaming</p></NuxtLink>
+          <NuxtLink to="/comunidadMentores"><p>Mentores</p></NuxtLink>
+          <NuxtLink to="/comunidadAmigos"><p>Amigos</p></NuxtLink>
         </div>
-        <div class="columna">
-          <h3>Comunidad</h3>
-          <ul>
-            <!-- Aquí colocarías tus 5 opciones de la comunidad -->
-          </ul>
-        </div>
-        <div class="columna">
-          <h3>Soporte</h3>
-          <ul>
-            <!-- Aquí colocarías tus 5 opciones de soporte -->
-          </ul>
+        <div class="flex flex-col gap-3">
+          <NuxtLink to="/soporte"><h5>Soporte</h5></NuxtLink>
+          <NuxtLink to="/soporte"><p>Inicio de sesión</p></NuxtLink>
+          <NuxtLink to="/soporte"><p>Juegos</p></NuxtLink>
+          <NuxtLink to="/soporte"><p>Compras</p></NuxtLink>
+          <NuxtLink to="/soporte"><p>Mis puntos</p></NuxtLink>
         </div>
       </div>
-      <div class="politicas">
-        <p>Política de Privacidad</p>
-        <p>Términos y Condiciones</p>
+    </div>
+    <div class="flex gap-3">
+      <Button :icon="'community'" :littleIcon="true"></Button>
+      <Button :icon="'community'" :littleIcon="true"></Button>
+      <Button :icon="'community'" :littleIcon="true"></Button>
+      <Button :icon="'community'" :littleIcon="true"></Button>
+      <Button :icon="'community'" :littleIcon="true"></Button>
+    </div>
+    <div class="flex flex-col gap-16">
+      <div class="flex justify-between">
+        <p class="bold">Bread | Bread | Bread | Bread | Bread</p>
+        <Button :icon="'community'" :littleIcon="true"></Button>
       </div>
-    </footer>
-  </template>
-  
-  <style scoped>
-  footer {
-    width: 100%;
-    height: 520px;
-    background-color: #F8F8F8; 
-    .footer-rights{
-        color: #6c6c6c;
-        
-    /* Estilos generales del footer */
-  }}
-  
-  .footer-left {
-    float: left;
-    width: 50%;
-    color: var(--primary);
-  }
-  
-  .footer-right {
-    float: right;
-    width: 50%;
-  }
-  
-  .iconos {
-    /* Estilos para los iconos */
-  }
-  
-  .columna {
-    float: left;
-    width: 33.33%;
-  }
-  
-  .politicas {
-    clear: both;
-  }
-  </style>
-  
+      <div class="flex justify-between">
+        <p>@2024 Steam. All rights reserved</p>
+        <div class="flex">
+          <p>Política y privacidad</p>
+          <p>Términos y condiciones</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Tag from "./mockComponents/Tag.vue";
+import Button from "./mockComponents/Button.vue";
+</script>
+
+<style lang="sass" scoped></style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,114 +1,127 @@
 <template>
-  <nav class="navbar">
-    <div class="navbar-left">
-      <NuxtLink to="/"><img src="/logo.png" alt="Logo" class="logo"></NuxtLink>
+  <div class="flex flex-col bg-white">
+    <div class="flex justify-between px-12 py-6 items-center">
+      <NuxtLink to="/"><img src="/steamLogo.svg" alt="logo" /></NuxtLink>
+      <div class="flex gap-12 h-fit items-center">
+        <Button :littleText="true" :color="'transparent'">Tienda</Button>
+
+        <NuxtLink to="/comunidadNoticias">
+            <Button :littleText="true" :color="'transparent'">Comunidad</Button>
+        </NuxtLink>
+        <NuxtLink to="/soporte">
+        <Button :littleText="true" :color="'transparent'">Soporte</Button>
+        </NuxtLink>
+        <Button :icon="'community'" :littleIcon="true"></Button>
+        <Button :littleText="true" :color="'gray'">Instala Steam</Button>
+
+        <template v-if="props.logged == false">
+          <NuxtLink to="/inicio">
+            <Button :littleText="true" :color="'primary'">Inicia Sesión</Button>
+          </NuxtLink>
+        </template>
+        <template v-if="props.logged ">
+          <img src="/user.png" alt="" class="w-14">
+        </template>
+
+      </div>
     </div>
-    <div class="navbar-center">
-      <NuxtLink to="/tienda" :class="{ 'nav-link': $route.path !== '/pagina1', 'nav-link-bold': $route.path === '/pagina1' }">Tienda</NuxtLink>
-      <NuxtLink to="/soporte" :class="{ 'nav-link': $route.path !== '/pagina1', 'nav-link-bold': $route.path === '/pagina1' }">Soporte</NuxtLink>
+    <div v-if="shop" class="flex flex-col gap-7 p-12">
+      <div class="flex justify-between">
+        <h2>Comunidad</h2>
+        <div class="flex gap-6">
+          <Button :icon="'community'"></Button>
+          <div class="flex relative">
+            <NuxtLink to="/shoppingCart">
+              <Button :icon="'cart'"></Button>
+            </NuxtLink>
+            <p v-if="itemsInCart.length > 0" class="flex justify-center items-center text-black bg-primary w-5 h-5 mono rounded-full absolute top-0 left-8">
+              {{ itemsInCart.length }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-11">
+        <NuxtLink to="/comunidadNoticias">
+            <Button :littleText="true" :color="nuevos">
+            Noticias
+          </Button>
+        </NuxtLink>
+
+        <NuxtLink to="/comunidadDiscussion">
+          <Button :littleText="true" :color="juegos">
+            Discusiones
+          </Button>
+        </NuxtLink>
+
+        <NuxtLink to="/comunidadStream">
+          <Button :littleText="true" :color="ofertas">
+            Streaming
+          </Button>
+        </NuxtLink>
+
+        <NuxtLink to="/comunidadAmigos">
+          <Button :littleText="true" :color="puntos">
+            Amigos
+          </Button>
+        </NuxtLink>
+        
+        <NuxtLink to="/comunidadMentores">
+          <Button :littleText="true" :color="puntos">
+            Mentores
+          </Button>
+        </NuxtLink>
+      </div>
     </div>
-    <div class="navbar-right">
-      <button class="icon-button">
-        <!-- Icono -->
-      </button>
-      <button class="login-button" @click="openModal"><!-- <Icon name="line-md:account" size="2em" color="black"></Icon> -->Iniciar Sesión</button>
-      <NuxtLink to="/install" class="install-button">Instalar steam</NuxtLink>
-    </div>
-    <!-- Aquí añadimos el modal -->
-    <LoginModal v-if="showModal" @close="closeModal" @openSignupModal="openSignupModal" />
-    <!-- Aquí añadimos el modal de registro -->
-    <SignupModal v-if="showSignupModal" @close="closeSignupModal" />
-  </nav>
+  </div>
 </template>
 
-<script>
-import LoginModal from '~/components/LoginModal.vue'; // Importar el componente modal
-import SignupModal from '~/components/SignupModal.vue'; // Importar el componente de registro
+<script setup>
+import { ref, watchEffect } from 'vue';
+import Button from "./mockComponents/Button.vue";
+import { inject } from 'vue';
 
-export default {
-  name: 'Navbar',
-  components: {
-    LoginModal,
-    SignupModal
-  },
-  data() {
-    return {
-      showModal: false,
-      showSignupModal: false
-    };
-  },
-  methods: {
-    openModal() {
-      this.showModal = true;
-    },
-    closeModal() {
-      this.showModal = false;
-    },
-    openSignupModal() {
-      this.showSignupModal = true;
-    },
-    closeSignupModal() {
-      this.showSignupModal = false;
-    }
+
+const itemsInCart = inject('itemsInCart');
+
+const props = defineProps({
+  shop: Boolean,
+  primary: String,
+  logged: Boolean
+});
+
+const nuevos = ref("");
+const juegos = ref("");
+const ofertas = ref("");
+const puntos = ref("");
+
+watchEffect(() => {
+  if (props.primary === 'nuevos') {
+    nuevos.value = "primary";
+    juegos.value = "";
+    ofertas.value = "";
+    puntos.value = "";
+  } else if (props.primary === 'juegos') {
+    juegos.value = "primary";
+    nuevos.value = "";
+    ofertas.value = "";
+    puntos.value = "";
+  } else if (props.primary === 'ofertas') {
+    ofertas.value = "primary";
+    nuevos.value = "";
+    juegos.value = "";
+    puntos.value = "";
+  } else if (props.primary === 'puntos') {
+    puntos.value = "primary";
+    nuevos.value = "";
+    juegos.value = "";
+    ofertas.value = "";
+  } else {
+    nuevos.value = "";
+    juegos.value = "";
+    ofertas.value = "";
+    puntos.value = "";
   }
-}
+});
 </script>
 
-<style lang="postcss" scoped>
-.navbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-}
-
-.navbar-left {
-  display: flex;
-  align-items: center;
-}
-
-.navbar-center {
-  display: flex;
-  gap: 40px;
-}
-
-.navbar-right {
-  display: flex;
-}
-
-.logo {
-  width: 50px;
-}
-
-.nav-link {
-  color: #000;
-  text-decoration: none;
-  margin-right: 20px;
-}
-
-.nav-link-bold {
-  font-weight: 300;
-}
-
-.icon-button {
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  padding: 5px;
-  color: #000;
-}
-
-.install-button {
-  background-color: #E6E6E6;
-  border-radius: 50px;
-  color: #6C6C6C;
-  border: none;
-  cursor: pointer;
-  padding: 10px 20px;
-  margin-left: 10px;
-}
-
-.install-button:hover {
-  background-color: #909090;
-}
-</style>
+<style lang="sass" scoped></style>
